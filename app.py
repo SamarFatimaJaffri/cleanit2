c9f6e2f72b6cd735683fc74d841deec8111f6b4c
Samar Fatima Jaffri <71835559+SamarFatimaJaffri@users.noreply.github.com>
2024-10-11 07:41:48 +0000

    hotfix: replace streamlit session with custom session class
    streamlit session variables refresh on each reload of the app, save variables in custom session class to avoid loss

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 from data_buffer import Data
 from pipeline import Pipeline
+from session import Session
 
 
 @st.cache_data
@@ -36,10 +37,8 @@ def get_api_key():
 
     # refresh after the pictures have been uploaded
     if st.button('Done'):
-        st.session_state.client = {
-            'provider': providers[provider],
-            'API_KEY': api_key
-        }
+        Session.provider = providers[provider]
+        Session.api_key = api_key
         st.rerun()
 
 
@@ -80,12 +79,8 @@ class App:
 
 
 if __name__ == '__main__':
-    # initialize empty client
-    if not st.session_state.get('client'):
-        st.session_state.client = {'provider': None, 'API_KEY': None}
-
     # set client
-    if not all(st.session_state.get('client').values()):
+    if not all((Session.provider, Session.api_key)):
         get_api_key()
 
     app = App()
--- data_buffer.py
+++ data_buffer.py
@@ -1,10 +1,4 @@
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
-        return cls._instances[cls]
+from metaclass import Singleton
 
 
 class Data(metaclass=Singleton):
